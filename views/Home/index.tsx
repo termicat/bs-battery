5174a658e1921cf4ec1d1ec0e6086638bc25b681
刘欢 <1829913225@qq.com>
2024-05-09 14:02:08 +0800

    refactor: 更新字段类别的值为recordCategory和fieldCategory的配置选项

--- views/Home/index.tsx
+++ views/Home/index.tsx
@@ -2,7 +2,8 @@ import { useTranslation } from "next-i18next";
 import { BsSdk } from "@/libs/bs-sdk/BsSdk";
 import { useRouter } from "next/router";
 import ConfigUI from "@/components/config-ui";
-import { scheme } from "@/config/options";
+import { fieldCategory, scheme } from "@/config/options";
+import { useState } from "react";
 
 // const bsSdk = new BsSdk({});
 
@@ -13,10 +14,30 @@ let glang = "zh";
 export default function App() {
   const router = useRouter();
   const [t, i18n] = useTranslation();
+  const [value, setValue] = useState(fieldCategory);
 
   return (
-    <div>
-      <ConfigUI scheme={scheme}></ConfigUI>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <div style={{ width: 340 }}>
+        <ConfigUI
+          scheme={scheme}
+          value={value}
+          onChange={(target, field, value) => {
+            setValue((prev) => {
+              const next: any = { ...prev };
+              next[target][field] = value;
+              return next;
+            });
+          }}
+        ></ConfigUI>
+      </div>
     </div>
   );
 }
