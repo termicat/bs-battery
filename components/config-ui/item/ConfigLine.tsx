61e2ae1aa3a87e44775bc03c885cb1a9cc2c7b4d
刘欢 <1829913225@qq.com>
2024-05-09 14:10:59 +0800

    refactor: 更新字段类别的值为recordCategory和fieldCategory的配置选项

--- components/config-ui/item/ConfigLine.tsx
+++ components/config-ui/item/ConfigLine.tsx
@@ -1,40 +1,15 @@
-import { Col, Input } from "@douyinfe/semi-ui";
+import { Col, Divider, Input } from "@douyinfe/semi-ui";
 import { useRef } from "react";
 import { ConfigItemProps } from "../ConfigItemProps";
 
-export type ConfigLineOptions = { };
+export type ConfigLineOptions = {};
 
-export type ConfigLineProps = ConfigItemProps<'line', ConfigLineOptions>;
+export type ConfigLineProps = ConfigItemProps<"line", ConfigLineOptions>;
 
-export default function ConfigLine(props:ConfigLineProps) {
-  const {
-    field,
-    label,
-    default: defaultValue,
-    value,
-    tip,
-    onChange,
-    target,
-    options
-  } = props;
-  const ref = useRef<any>();
-
+export default function ConfigLine(props: ConfigLineProps) {
   return (
-    <Col span={12} style={{ padding: "5px", paddingTop: "10px" }}>
-      <div style={{ fontSize: "14px", fontWeight: "bold", color: "#333" }}>
-        {label}
-      </div>
-      <Input
-        style={{ marginTop: "5px" }}
-        ref={ref}
-        defaultValue={defaultValue}
-        value={value}
-        type="text"
-        onChange={(v) => onChange(target, field, v)}
-      />
-      <div style={{ fontSize: "12px", marginTop: "2px", color: "#666" }}>
-        {tip}
-      </div>
+    <Col span={24} style={{ padding: "5px", paddingTop: "10px" }}>
+      <Divider></Divider>
     </Col>
   );
 }
