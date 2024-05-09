fece0c8e194cd59ce853ea8abce58822f2ec84bc
刘欢 <1829913225@qq.com>
2024-05-09 14:19:18 +0800

    fix: 布局优化

--- views/Home/index.tsx
+++ views/Home/index.tsx
@@ -21,34 +21,36 @@ export default function App() {
     <div
       style={{
         display: "flex",
-        flexDirection: "column",
+        flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
       }}
     >
-      <div style={{ width: 340 }}>
-        <ConfigUI
-          scheme={scheme}
-          value={value}
-          onChange={(target, field, value) => {
-            console.log("Root onChange", target, field, value);
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div style={{ width: 340 }}>
+          <ConfigUI
+            scheme={scheme}
+            value={value}
+            onChange={(target, field, value) => {
+              console.log("Root onChange", target, field, value);
 
-            target[field] = value;
-            setValue({ ...target });
+              target[field] = value;
+              setValue({ ...target });
+            }}
+          ></ConfigUI>
+        </div>
+        <pre
+          style={{
+            width: 340,
+            marginLeft: 10,
+            padding: 10,
+            border: "1px solid #eee",
           }}
-        ></ConfigUI>
+        >
+          {JSON.stringify(value, null, 2)}
+        </pre>
       </div>
-      <pre
-        style={{
-          margin: 10,
-          padding: 10,
-          border: "1px solid #eee",
-          width: 340,
-        }}
-      >
-        {JSON.stringify(value, null, 2)}
-      </pre>
     </div>
   );
 }
