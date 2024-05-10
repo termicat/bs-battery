7d5fdf354c27344d1b845ba9fe5d6a0a7b4d4f4f
刘欢 <1829913225@qq.com>
2024-05-10 14:50:18 +0800

    refactor: 优化ConfigSelectTheme组件的样式

--- components/config-ui/item/ConfigSelectTheme.tsx
+++ components/config-ui/item/ConfigSelectTheme.tsx
@@ -110,6 +110,13 @@ const ColorItem = styled.div`
   border: 0;
   margin: 0;
   padding: 0;
+
+  &:first-child {
+    border-radius: 4px 0 0 4px;
+  }
+  &:last-child {
+    border-radius: 0 4px 4px 0;
+  }
 `;
 
 function ThemeColors(props: ThemeColorProps) {
