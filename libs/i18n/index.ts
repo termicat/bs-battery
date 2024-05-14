1782b54343cc17cdc2688b741f7918a7d2e658e0
刘欢 <1829913225@qq.com>
2024-05-14 20:06:07 +0800

    chore: 优化 i18n

--- libs/i18n/index.ts
+++ libs/i18n/index.ts
@@ -1,10 +1,16 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import en from "./locales/en/common.json";
+import zh from "./locales/zh/common.json";
 
-i18n.use(initReactI18next).init({
+const opt = {
   resources: {
-    en: { translation: require("../public/locales/en/common.json") },
-    zh: { translation: require("../public/locales/zh/common.json") },
+    en: {
+      translation: en,
+    },
+    zh: {
+      translation: zh,
+    },
     // 添加更多语言...
   },
   lng: "zh", // 默认语言
@@ -13,6 +19,9 @@ i18n.use(initReactI18next).init({
   interpolation: {
     escapeValue: false,
   },
-});
+};
+i18n.use(initReactI18next).init(opt);
+
+console.log(opt);
 
 export default i18n;
