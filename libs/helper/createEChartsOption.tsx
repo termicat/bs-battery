6a1c38ee7dd8d8075d043cd19c2a3954aa32f855
刘欢 <1829913225@qq.com>
2024-05-15 18:30:53 +0800

    chore: optimize tooltip formatting in createEChartsOption

--- libs/helper/createEChartsOption.tsx
+++ libs/helper/createEChartsOption.tsx
@@ -2,39 +2,29 @@ export function createEChartsOption(data: any[][], configRoot: any) {
   const common = {
     theme: configRoot.selectTheme,
     tooltip: {
-      trigger: "item",
-      formatter: function (p: any) {
-        // console.log(p);
-        // 在 tooltip 中显示的内容可以根据需求自定义
-        return `<div style="display: flex;">
-        <li style="color:${p.color};font-size: 30px;margin-right: -25px"></li>
-        ${p.name}: ${p.value[p.dataIndex]}
-      <div>`;
-      },
+      // trigger: "item",
+      // formatter: function (p: any) {
+      //   console.log(p.seriesIndex, p.value[p.seriesIndex], p);
+      //   // 在 tooltip 中显示的内容可以根据需求自定义
+      //   return `<div style="display: flex;">
+      //   <li style="color:${p.color};font-size: 30px;margin-right: -25px"></li>
+      //   ${p.name}: ${p.value[p.dataIndex]}
+      // <div>`;
+      // },
     },
   };
   const seriesCommon = {
-    // emphasis: {
-    //   // 设置鼠标悬停时的样式
-    //   itemStyle: {
-    //     // borderColor: "", // 设置边框颜色
-    //     // borderWidth: 10, // 设置边框宽度
-    //     focus: "self",
-    //   },
-    // },
-    // areaStyle: {
-    //   opacity: 0.1,
-    // },
-    // emphasis: {
-    //   // 设置鼠标悬停时的样式
-    //   itemStyle: {
-    //     borderWidth: 10, // 设置边框宽度
-    //     focus: "self",
-    //   },
-    // },
-    // areaStyle: {
-    //   opacity: 0.3,
-    // },
+    emphasis: {
+      // 设置鼠标悬停时的样式
+      itemStyle: {
+        // borderColor: "", // 设置边框颜色
+        // borderWidth: 10, // 设置边框宽度
+        focus: "self",
+      },
+    },
+    areaStyle: {
+      opacity: 0.3,
+    },
   };
   const type = configRoot.mapType;
   if (type === "fieldCategory") {
@@ -59,6 +49,19 @@ export function createEChartsOption(data: any[][], configRoot: any) {
           data: data[0]?.slice(1)?.map?.((item, index) => {
             return {
               ...seriesCommon,
+              // tooltip: {
+              //   show: true,
+              //   formatter: function (p: any) {
+              //     console.log(p.seriesIndex, p.value[p.seriesIndex], p);
+              //     // 在 tooltip 中显示的内容可以根据需求自定义
+              //     return `<div style="display: flex;">
+              //     <li style="color:${
+              //       p.color
+              //     };font-size: 30px;margin-right: -25px"></li>
+              //     ${p.name}: ${p.value[p.dataIndex]}
+              //   <div>`;
+              //   },
+              // },
               value: data.slice(1).map((item) => item[index + 1].value),
               name: item.text,
               label: when(configRoot.chartOptions.includes("showDataLabel"), {
@@ -91,9 +94,20 @@ export function createEChartsOption(data: any[][], configRoot: any) {
         {
           name: "Data",
           type: "radar",
-          data: data.slice(1).map((item) => {
+          data: data.slice(1).map((item, index) => {
             return {
               ...seriesCommon,
+              // tooltip: {
+              //   show: true,
+              //   formatter: function (p: any) {
+              //     console.log(index, p.value[index], p);
+              //     // 在 tooltip 中显示的内容可以根据需求自定义
+              //     return `<div style="display: flex;">
+              //     <li style="color:${p.color};font-size: 30px;margin-right: -25px"></li>
+              //     ${p.name}: ${p.value[index]}
+              //   <div>`;
+              //   },
+              // },
               value: item.slice(1).map((item) => item.value),
               name: item[0].text,
               label: when(configRoot.chartOptions.includes("showDataLabel"), {
