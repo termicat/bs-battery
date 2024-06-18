795fdfa5b7b8f8d56012ade1b1e8063606425b79
刘欢 <1829913225@qq.com>
2024-06-18 13:28:37 +0800

    chore: add vcharts

---
+++ libs/vcharts/index.tsx
@@ -0,0 +1,34 @@
+import * as echarts from "echarts";
+import { useEffect, useRef } from "react";
+import { withResizeDetector } from "react-resize-detector";
+
+export default withResizeDetector(function ECharts(props: any) {
+  const { option, width } = props;
+  const chartRef = useRef<HTMLDivElement>(null);
+  const echartsInstance = useRef<echarts.ECharts>();
+  useEffect(() => {
+    // 接听元素宽度变化
+
+    const isDark = document.body.getAttribute("theme-mode") === "dark";
+
+    const chart = echarts.init(chartRef.current!, isDark ? "dark" : "light");
+    try {
+      chart.setOption(option);
+    } catch (error) {
+      console.error("setOption error", error);
+    }
+    if (props.refInstance) props.refInstance.current = chart;
+    echartsInstance.current = chart;
+    return () => {
+      chart.dispose();
+    };
+  }, [option, props.refInstance, props.themeMode]);
+
+  useEffect(() => {
+    if (echartsInstance.current) {
+      echartsInstance.current.resize();
+    }
+  }, [width]);
+
+  return <div ref={chartRef} style={{ width: "100%", height: "100%" }}></div>;
+});
