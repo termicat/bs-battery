bb0fddd7271f72c8fa9d71e7b704380d7ff3ab32
刘欢 <1829913225@qq.com>
2024-06-20 14:06:18 +0800

    refactor: Optimize radar chart data processing

--- libs/helper/createVChartsOption.tsx
+++ libs/helper/createVChartsOption.tsx
@@ -4,93 +4,40 @@ export function createVChartsOption(data: any[][], configRoot: any) {
   const colors = theme.light.find(
     (item) => item.value === configRoot.selectTheme
   )?.label;
+  const data2 = data
+  .slice(1)
+  .filter((item) => item[0].text)
+  .map((row, index) => {
+    const cols = data[0].slice(1);
+    const name = row[0].text;
 
+    return {
+      values: row.slice(1).map((item, colIndex) => {
+        const col = cols[colIndex];
+        return {
+          cate: name,
+          value: item.value,
+          type: col.text,
+        };
+      }),
+    };
+  })
+  .reduce(
+    (acc, cur: any) => {
+      acc.values = acc.values.concat(cur.values);
+      return acc;
+    },
+    {
+      values: [],
+    }
+  );
+  const max = calculateMaxValues(data2.values);
   const spec = {
     type: "radar",
     color: colors,
     background: "rgba(0,0,0,0)",
-    data: data
-      .slice(1)
-      .filter((item) => item[0].text)
-      .map((row, index) => {
-        const cols = data[0].slice(1);
-        const name = row[0].text;
-
-        return {
-          values: row.slice(1).map((item, colIndex) => {
-            const col = cols[colIndex];
-            return {
-              cate: name,
-              value: item.value,
-              type: col.text,
-            };
-          }),
-        };
-      })
-      .reduce(
-        (acc, cur: any) => {
-          acc.values = acc.values.concat(cur.values);
-          return acc;
-        },
-        {
-          values: [],
-        }
-      ),
-    // data: data[0].slice(1).map((col, colIndex) => {
-    //   return {
-    //     fields: {
-    //       type: {
-    //         type: "ordinal",
-    //         sortIndex: 1,
-    //       },
-    //       value: {
-    //         type: "linear", // 数据维度 value 的类型是连续类型
-    //         // domain: [0, 10000], // 会提出 value 值不在 0-10000内的数据
-    //       },
-    //     },
-    //     values: data
-    //       .slice(1)
-    //       .map((item) => {
-    //         return {
-    //           cate: item[0].text,
-    //           value: item[colIndex + 1].value,
-    //           type: col.text,
-    //         };
-    //       })
-    //       .filter((item: any) => typeof item.value === "number")
-    //       .sort((a: any, b: any) => a.value - b.value),
-    //   };
-    // }),
-    // data: [
-    //   {
-    //     values: data[0]
-    //       .slice(1)
-    //       .map((col, colIndex) => {
-    //         return {
-    //           values: data.slice(1).map((item) => {
-    //             return {
-    //               cate: item[0].text,
-    //               value: item[colIndex + 1].value,
-    //               type: col.text,
-    //             };
-    //           }),
-    //         };
-    //       })
-    //       .reduce((acc, cur: any) => acc.concat(cur.values), [])
-    //       .filter((item: any) => typeof item.value === "number")
-    //       .sort((a: any, b: any) => a.value - b.value),
-    //     fields: {
-    //       type: {
-    //         type: "ordinal",
-    //         sortIndex: 1,
-    //       },
-    //       value: {
-    //         type: "linear", // 数据维度 value 的类型是连续类型
-    //         // domain: [0, 10000], // 会提出 value 值不在 0-10000内的数据
-    //       },
-    //     },
-    //   },
-    // ],
+    data: data2,
+    padding: 0,
     valueField: "value",
 
     ...(configRoot.mapType === "recordCategory"
@@ -112,7 +59,7 @@ export function createVChartsOption(data: any[][], configRoot: any) {
       {
         orient: "radius", // radius axis
         zIndex: 100,
-        min: 0,
+        max,
         domainLine: {
           visible: false,
         },
@@ -124,12 +71,6 @@ export function createVChartsOption(data: any[][], configRoot: any) {
             lineWidth: 4,
           },
           visible: configRoot.chartOptions.includes("showDataLabel"),
-          // formatMethod:
-          //   configRoot.axisValue === "single"
-          //     ? (val: any) => {
-          //         return val * 100 + "%";
-          //       }
-          //     : undefined,
         },
         grid: {
           smooth: false,
@@ -172,19 +113,11 @@ function when(cond: any, value: any) {
   return cond ? value : undefined;
 }
 
-function calculateMaxValues(series: any[]) {
-  if (series.length === 0) return [];
-
-  var numDimensions = series[0].value.length;
-  var maxValues = new Array(numDimensions).fill(-Infinity);
-
-  series.forEach(function (serie) {
-    serie.value.forEach(function (val: number, index: number) {
-      if (val > maxValues[index]) {
-        maxValues[index] = val;
-      }
-    });
-  });
-
-  return maxValues;
+function calculateMaxValues(data2: any[]) {
+  return data2.reduce((acc, cur) => {
+    return Math.max(
+      acc,
+      cur.value
+    );
+  }, 0);
 }
